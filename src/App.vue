0cdf89f69073160688c143bc1fd2394abae770b9
Avni Bafna <99932053+AvniBafna@users.noreply.github.com>
2024-12-08 06:04:15 +0530

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
-  <div id="app">
-    <router-view />
+  <div id="app" class="page-container">
+    <!-- Navigation bar or any other header components can go here -->
+    <router-view></router-view> <!-- This will render the routed components -->
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-
-export default defineComponent({
+<script>
+export default {
   name: 'App',
-});
+};
 </script>
+
+<style scoped>
+/* Import the global CSS file */
+@import './App.css';
+</style>
