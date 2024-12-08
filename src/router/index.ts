7a562d8bc363f1882f48a33f21205c86b469b967
Avni Bafna <99932053+AvniBafna@users.noreply.github.com>
2024-12-07 16:33:13 -0800

    Add files via upload

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,26 +1,23 @@
-import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router';
-import CreateContract from "../components/CreateContract.vue";
-import Home from '../components/Home.vue';
+// router/index.js
+import { createRouter, createWebHistory } from 'vue-router';
+import Dashboard from '../components/Dashboard.vue'; // Adjust path if necessary
+import CredentialForm from '../components/CredentialForm.vue'; // Adjust path if necessary
+// import ViewCredential from '../components/ViewCredential.vue'; // Adjust path if necessary
+import CreateContract from '../components/CreateContract.vue'; // Adjust path if necessary
+import trackCredential from '../components/TrackStatus.vue'; // Adjust path if necessary
+import Login from '../components/Login.vue'; // Adjust path if necessary
 
-const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home,
-  },
-  {
-    path: '/create-contract',
-    name: 'CreateContract',
-    component: CreateContract,
-  },
-  {
-    path: '/:pathMatch(.*)*',
-    redirect: '/',
-  },
+const routes = [
+  { path: '/', name: 'Dashboard', component: Dashboard },
+  { path: '/login', name: 'Login', component: Login },
+  { path: '/verify', name: 'CredentialForm', component: CredentialForm },
+//  { path: '/view', name: 'ViewCredential', component: ViewCredential },
+  { path: '/issue', name: 'CreateContract', component: CreateContract },
+  { path: '/verify-track', name: 'TrackStatus', component: trackCredential },
 ];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes,
 });
 
